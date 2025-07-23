add8d30cc37979a6cee57de0fd0ed1b069e07a4a
CodexLante <allomwas@gmail.com>
2025-07-23 23:59:54 +0300

    SCRUM-26:loop over each possible diagonal

--- Chess-App/src/Components/Bishop.jsx
+++ Chess-App/src/Components/Bishop.jsx
@@ -28,7 +28,88 @@ function Bishop({position, isSlected, onSelect, board, color}){
         const piece = board.find(p => p.x === x && p.y === y);
 
         return piece && piece.color !== color;
-    };  
+    };
+    
+    const calculateValidMoves = () => {
+        const movements = [];
+        const x = position.x;
+        const y = position.y;
 
 
-}
+    }
+       // Check top-right diagonal
+
+    for (let i = 1; i < 8; i++) {
+
+      const newX = x + i;
+      const newY = y + i;
+
+      if (!isOnBoard(newX, newY)) break;
+      if (isSquareEmpty(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'move' });
+      } else if (isEnemyPiece(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'capture' });
+        break;
+      } else {
+        break;
+      }
+    }
+   // Check bottom-right diagonal
+
+    for (let i = 1; i < 8; i++) {
+
+      const newX = x + i;
+      const newY = y - i;
+
+      if (!isOnBoard(newX, newY)) break;
+      if (isSquareEmpty(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'move' });
+      } else if (isEnemyPiece(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'capture' });
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // Check top-left diagonal
+    for (let i = 1; i < 8; i++) {
+
+      const newX = x - i;
+      const newY = y + i;
+
+      if (!isOnBoard(newX, newY)) break;
+      if (isSquareEmpty(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'move' });
+      } else if (isEnemyPiece(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'capture' });
+        break;
+
+      } else {
+        break;
+      }
+    }
+
+    // Check bottom-left diagonal
+
+    for (let i = 1; i < 8; i++) {
+
+      const newX = x - i;
+      const newY = y - i;
+
+      if (!isOnBoard(newX, newY)) break;
+
+      if (isSquareEmpty(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'move' });
+        
+      } else if (isEnemyPiece(newX, newY)) {
+        movements.push({ x: newX, y: newY, type: 'capture' });
+        break;
+      } else {
+        break;
+      }
+    }
+
+    return movements;
+}; 
+
