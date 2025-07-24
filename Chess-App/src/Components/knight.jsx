53b04a4ff3bbc110deeb530c33bb180e27ea41fb
joan@joan <mugurejoan3@gmail.com>
2025-07-24 19:05:15 +0300

    SCRUM-46:importing knight images >

--- Chess-App/src/Components/knight.jsx
+++ Chess-App/src/Components/knight.jsx
@@ -1,3 +1,18 @@
 import whiteKnight from '../assets/wn.png';
 import blueKnight from "../assets/bn.png";
 
+function Knight({ position, isSelected, onSelect, board, color }) {
+
+    const isOnBoarimportd = (x, y) => x >= 0 && x < 8 && y >= 0 && y < 8;
+
+  const isFriendlyPiece = (x, y) => {
+    const piece = board.find(p => p.x === x && p.y === y);
+    return piece && piece.color === color;
+  };
+
+  const isEnemyPiece = (x, y) => {
+    const piece = board.find(p => p.x === x && p.y === y);
+    return piece && piece.color !== color;
+  };
+}
+
